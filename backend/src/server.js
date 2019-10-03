7f9f54c02fc552e336c65a3509442e5913e8359f
JoaoVSouto <joaovitorss99@hotmail.com>
2019-10-03 14:29:45 -0300

    Adds route to serve images

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
@@ -15,6 +16,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(require('./routes'));
 
 app.listen(3333, () => console.log('server listening on 3333...'));
